cc3a3d9adfb2ef3ca929473209295afd5f228a04
unknown <alek.bisevac@gmail.com>
2021-07-03 21:40:40 +0200

    added one more version of exercise, multipage gitsearch

--- AJAX/exercises/task3-github-searcher/main.js
+++ AJAX/exercises/task3-github-searcher/main.js
@@ -10,17 +10,23 @@ let $repositories = []; // ubaci sve repozitorijume od user-a!
 $searchInput.keydown(function (e) {
   if (e.keyCode == 13) {
     let $subject = $searchInput.val();
+    // šaljemo zahtev serveru za pretragu user-a na GitHub-u
     $.ajax({
       url: `https://api.github.com/search/users?q=${$subject}+in:user`,
       type: "GET",
       dataType: "json",
     }).done(function (response) {
+      // dobijeni rezultat kopiramo u novi niz &objects
       $objects = response.items.map((x) => x);
       $resultWrapper.html("");
+      // za svaki element unutar $objects stvaramo novi DIV i ubacujemo potrebne podatke i prikazujemo ih
       $objects.forEach((element, index) => {
         let $responseBox = $("<div>");
         $responseBox.html(
-          `<div class="img-handler"><img src="${element.avatar_url}"></div><h3 data-key=${element.id}>${element.login}</h3>`
+          `<div class="img-handler">
+            <img src="${element.avatar_url}">
+          </div>
+          <h3 data-key=${element.id}>${element.login}</h3>`
         );
         $resultWrapper.append($responseBox);
         $responseBox.addClass("response-box");
@@ -28,6 +34,7 @@ $searchInput.keydown(function (e) {
 
       /* dajemo event listener za svaki h3 */
       $(".response-box h3").click(function (e) {
+        // prikazujemo DIV sa našim rezultatima i dodeljujemo dva osluškivača za ESC i klik, koji sakrivaju naš DIV
         $showProfile.removeClass("show-hide");
         $("body").keydown(function (e) {
           if (e.keyCode == 27) {
@@ -47,16 +54,29 @@ $searchInput.keydown(function (e) {
           type: "GET",
           dataType: "json",
         }).done(function (response) {
+          // ponovo pravimo kopiju odgovora u novi niz
           $repositories = response.map((x) => x);
           $repInfo.html("");
+          // za svaki element unutar $repositories stvaramo novi DIV i ubacujemo potrebne podatke i prikazujemo ih
           $repositories.forEach((element) => {
             let $repItem = $("<div>");
-            $repItem.html(`<div class="rep-img"><img src="github.png"></div><a href="${element.html_url}" target="_blank"><h4 class="rep-name">${element.name}</h4></a>
-            <div class="rep-description"><p>${element.description}</p></div>
-            <div class="stars">&#11088; ${element.stargazers_count}</div>`);
+            $repItem.html(`
+            <div class="rep-img">
+              <img src="github.png">
+            </div>
+            <a href="${element.html_url}" target="_blank">
+              <h4 class="rep-name">${element.name}</h4>
+            </a>
+            <div class="rep-description">
+              <p>${element.description}</p>
+            </div>
+            <div class="stars">
+              &#11088; ${element.stargazers_count}
+            </div>`);
             $repInfo.append($repItem);
             $repItem.addClass("rep-item");
           });
+          //info usera za header, u prozoru za prikaz direktorijuma
           $userImg.html(`<img src="${obj.avatar_url}">`);
           $userName.text(obj.login);
         });
